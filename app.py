75377211b647bb14699ddeb7f518c4345dc4962c
Yovanne <yovanne.ponnou@gmail.com>
2025-03-12 12:23:39 +0100

    Refactor MySQL configuration into a separate Config class and clean up app.py

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 from flask import Flask
 from flask_mysqldb import MySQL
+from config import Config
 
 app = Flask(__name__)
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'netflix'
- 
+app.config.from_object(Config)
 mysql = MySQL(app)
 
 @app.route("/")
-
 def home():
     return "Hello, Flask!"

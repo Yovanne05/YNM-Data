ed47fab87b073cea1bc399bf5e0d74ef4d8cd12d
Yovanne05 <yovanne.ponnou@gmail.com>
2025-03-18 10:06:03 +0100

    debug db

--- app.py
+++ app.py
@@ -1,33 +1,50 @@
 from flask import Flask, jsonify
-from flask_mysqldb import MySQL
+import pymysql
 from flask_cors import CORS
 from config import Config
 from controllers.serie_controller import series_controller
 
+pymysql.install_as_MySQLdb()
+
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.config.from_object(Config)
-mysql = MySQL(app)
+
+def get_db_connection():
+    return pymysql.connect(
+        host=Config.MYSQL_HOST,
+        user=Config.MYSQL_USER,
+        password=Config.MYSQL_PASSWORD,
+        database=Config.MYSQL_DB,
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
 app.register_blueprint(series_controller)
 
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
 @app.route('/tables', methods=['GET'])
 def get_tables():
-    cursor = mysql.connection.cursor()
     try:
-        cursor.execute("SHOW TABLES")
-        tables = [table[0] for table in cursor.fetchall()]
+        conn = get_db_connection()
+        with conn.cursor() as cursor:
+            cursor.execute("SHOW TABLES")
+            tables = [table['Tables_in_' + Config.MYSQL_DB] for table in cursor.fetchall()]
 
         result = {}
         for table in tables:
-            cursor.execute("DESCRIBE `{}`".format(table))
-            columns = [column[0] for column in cursor.fetchall()]
-            result[table] = columns
+            with conn.cursor() as cursor:
+                cursor.execute(f"DESCRIBE `{table}`")
+                columns = [column['Field'] for column in cursor.fetchall()]
+                result[table] = columns
 
         return jsonify(result)
-    
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
     finally:
-        cursor.close() 
+        conn.close()
+

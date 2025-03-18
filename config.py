f1d3b9fb3e6cb8e2dc2042dd8d0ef1eb26c905c4
Yovanne <yovanne.ponnou@gmail.com>
2025-03-18 20:25:57 +0100

    Merge

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    MYSQL_HOST = 'database-etudiants.iut.univ-paris8.fr'
-    MYSQL_USER = 'dutinfopw201655'
-    MYSQL_PASSWORD = 'hevenequ'
-    MYSQL_DB = 'dutinfopw201655'
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'netflixdb'

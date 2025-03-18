b977ba82d2fc4390d2bc88ac73990e3aed503050
Yovanne05 <yovanne.ponnou@gmail.com>
2025-03-18 13:33:18 +0100

    ajout attribut dans modèle netflix

--- models/utilisateur_model.py
+++ models/utilisateur_model.py
@@ -1,11 +1,13 @@
 from utils.statut_utilisateur import statut_autorise
 class Utilisateur:
-    def __init__(self, id_utilisateur: int, nom: str, prenom: str, age: int, pays_residence: str, statut: str):
+    def __init__(self, id_utilisateur: int, nom: str, prenom: str, age: int, pays_residence: str, email: str, numero: str, statut: str):
         self.id_utilisateur = id_utilisateur
         self.nom = nom
         self.prenom = prenom
         self.age = age
         self.pays_residence = pays_residence
+        self.email = email
+        self.numero = numero
         self.statut = statut
 
     @classmethod
@@ -21,6 +23,8 @@ class Utilisateur:
             prenom=data['prenom'],
             age=data['age'],
             pays_residence=data['paysResidence'],
+            email=data['email'],
+            numero=data['numero'],
             statut=statut_abonnement
         )
 
@@ -31,5 +35,7 @@ class Utilisateur:
             'prenom': self.prenom,
             'age': self.age,
             'pays_residence': self.pays_residence,
+            'email': self.email,
+            'numero': self.numero,
             'statut_abonnement': self.statut
         }

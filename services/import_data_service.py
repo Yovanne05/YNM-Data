739505e2d7ce9ec39c7f2d98c036d338d2eb1b58
NikolaPetricevic <petricevicnikola64@gmail.com>
2025-03-25 22:23:52 +0100

    Commit Vérification du format de fichier

--- services/import_data_service.py
+++ services/import_data_service.py
@@ -2,4 +2,8 @@ from flask import Request
 
 def save_file(request: Request) -> None:
     file = request.files['file']
+
+    if file.mimetype not in ["text/csv", "application/vnd.ms-excel"]:
+        raise Exception("Format de fichier non valide")
+
     file.save(f"utils/import.csv")
